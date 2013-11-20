da7d64e85847944ce4b0d8cc14d059fd6712873b
btwright <btw3ta@virginia.edu>
2013-11-20 03:07:49 -0500

    Not as much done as I would have liked, but it's something. I created structs for players, blocks, and map, and a field for the global RGB tally. I also added the most minimal skeleton of an addInventory function, since that directly manipulates the db's, and a read command function- it doesn't really make sense to create a whole struct when we'll need a function to sift through the input anyways. I also added some comments explaining some of the deficiencies in my knowledge.

--- database.rs
+++ database.rs
@@ -1,3 +1,36 @@
 //Need: world-tracking rgb, player db (connection info, rgb, attributes, position), map db (size, capacity, active positions), block info db (makeup, eat, destroy, take, drop), command db (function library for all commands)
 
 
+use std::*
+
+static mut worldRGB: int[] = [0, 0, 0];
+
+struct map {
+   mut field: bool[bool[]]
+   size: int,//it may be simpler to just fold this whole thing into Rust's native vector type, let it figure out when to get bigger
+   capacity: float
+}
+
+struct blockInfo {
+   makeup: int[red, green, blue],
+   //need to represent eat, destroy, etc. as functions, blockInfo needs to be able to get them
+}
+
+struct playerRGB {
+   mut red: int,
+   mut green: int,
+   mut blue: int,
+   mut health: int,
+   mut attack: int,
+   mut defense: int,
+   mut invSize: int,
+   mut inv: blockInfo[]//not sure if this is correct syntax for declaring a variable size array- or if that's possible without simply destroying the old one and replacing it.
+}
+
+fn addInventory(~player: playerRGB, ~block: blockInfo) {
+
+}
+
+fn readCommand(args: string[], target) {
+   
+}
