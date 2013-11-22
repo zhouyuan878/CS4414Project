9dd699584d17233f70a5b474ace1e49bd9333ccf
newbatthis <yz2ak@virginia.edu>
2013-11-22 10:59:45 -0500

    Update interaction.rs

--- interaction.rs
+++ interaction.rs
@@ -1,1 +1,11 @@
 //contains all connection functions, manages p-p and p-s interactions
+
+use std::*
+
+fn updateWorld(args: command) {
+//matches the command and make the appropriate update to the world
+}
+
+fn updatePlayer(args: command, player) {
+//matches the command and make the appropriate update to the player
+}
