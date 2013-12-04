77067b9d465a7707b0bd2a20a303a163eebc847d
newbatthis <yz2ak@virginia.edu>
2013-12-04 13:53:54 -0500

    Update game.rs

--- game.rs
+++ game.rs
@@ -1,29 +1,26 @@
-
-
 mod database;
 
-
+use std::*;
 
 //check if command is allowed
 fn allowed_command(cmd: ~str) -> bool {
         let allowed_cmdlist: ~[~str] = ~[~"mix", ~"eat", ~"take", ~"drop", ~"move", ~"look", ~"create"];
-        if allowed_cmdlist.contains(&cmd) {return true;}
+        if allowed_cmdlist.contains(&cmd) {println("ya"); return true;}
         else {return false;}
 }
 
 //check if command has the ocrrect number of parameters; to be modified
 fn check_command(command: ~[~str]) {
-    let cmd: ~str = command[0];
     
-    if (cmd == ~"eat") ||  (cmd == ~"take") || (cmd == ~"drop") || (cmd == ~"look") || (cmd == ~"create") {
-        if (command[1] != "") 
+    if (command[0] == ~"eat") ||  (command[0] == ~"take") || (command[0] == ~"drop") || (command[0] == ~"look") || (command[0] == ~"create") {
+        if (command.len() > 1) 
         {print("No parameter should be used for your command!");
         return;
                       }
         }
-    
-    else if (cmd == ~"mix") ||  (cmd == ~"move") {
-        if (command[1] == "") 
+
+    else if (command[0] == ~"mix") ||  (command[0] == ~"move") {
+        if (command.len() == 1) 
         {print("Use a parameter for your command!");
         return;
         }
@@ -32,14 +29,15 @@ fn check_command(command: ~[~str]) {
   
 //need to be modified to handle commands with different parameters
 fn handle_command(cmd: ~[~str]) {
-        let command: ~str = cmd[0]; 
+        let command: ~str = cmd[0].clone(); 
+	println("yay");
         if !allowed_command(command){
            //(redundant) print("command not allowed!");
            return;
 }
        else {
          check_command(cmd);
-         database::cmd;
+	 //database::cmd;
          }
   }
   
@@ -47,6 +45,23 @@ fn handle_command(cmd: ~[~str]) {
 
 
 
+fn main() {
+
+        static CMD_PROMPT: &'static str = "Enter your command:";
+        
+        
+        loop {        
+                print(CMD_PROMPT);
+                let command = io::stdin().read_line();
+		let cmd: ~[~str] = command.split_iter(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();  
+                handle_command(cmd);
+        }
+}
+  
+  
+ 
+ 
+  
 fn main() {
 
         static CMD_PROMPT: &'static str = "Enter your command:";
