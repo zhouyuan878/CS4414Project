431370c1248ae9b0f579c082078842fc3ceedd31
zhouyuan878 <yz5ky@virginia.edu>
2013-12-05 21:03:20 -0500

    Update main_new.rs

--- main_new.rs
+++ main_new.rs
@@ -1,5 +1,19 @@
-use std::{io, run, os, path, libc};
+extern mod extra;
 
+
+use std::{io, run, os, path, libc, str};
+
+
+use std::rt::io::*;
+use std::io::ReaderUtil;
+use std::rt::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::io::println;
+use std::cell::Cell;
+use std::task;
+
+static PORT: int = 4414;
+static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static mut visitor_count: uint = 0;
 
 //check if command is allowed
 fn allowed_command(cmd: ~str) -> bool {
@@ -58,7 +72,30 @@ fn print_object(input: int) {
 
 
 fn main() {
+        let socket = net::tcp::TcpListener::bind(SocketAddr {ip: Ipv4Addr(127,0,0,1), port: PORT as u16});
+		
+		 println(fmt!("Listening on tcp port %d ...", PORT));
+        let mut acceptor = socket.listen().unwrap();
   
+        for stream in acceptor.incoming() {
+        println!("Saw connection!");
+        let stream = Cell::new(stream);
+        // Start a task to handle the connection
+        do task::spawn {
+            let mut stream = stream.take();
+            let mut buf = [0, ..500];
+            stream.write("welcome\n".as_bytes());
+            loop {
+                stream.read(buf);
+                let request_str = str::from_utf8(buf);
+                stream.write("Thanks for telling me about ::".as_bytes());
+                stream.write(buf);
+            }
+        }
+       }
+       
+	   
+	   
         static CMD_PROMPT: &'static str = "Enter your command:";
 
         let mut WorldMap: [[int, ..100], ..100] = [[0, ..100], ..100];
